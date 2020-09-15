70eaef0b08c7c446364ae7ea52a557ac86328b42
Matt <matthew.westover@gmail.com>
2020-09-14 22:56:34 -0600

    Initial Commit

---
+++ DiceCalculator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DiceCalculator
+//
+//  Created by Matt Westover on 9/14/20.
+//  Copyright © 2020 Matt Westover. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
